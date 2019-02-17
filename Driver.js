1e5547518dd5074b831392ef028e4ddd2f7f6976
Isaac Khan <Isaac.Khan@usm.edu>
2019-02-17 17:32:18 -0600

    adjusting function and variable names

--- Driver.js
+++ Driver.js
@@ -1,4 +1,26 @@
 function calculate()
 {
-    var a = document.querySelector("#value1").value;
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+    var calculate;
+
+    if(op == "add")
+    {
+        calculate = a + b;
+    }
+    else if (op = "min")
+    {
+        calculate = a - b;
+    }
+    else if (op = "div")
+    {
+        calculate = a / b;
+    }
+    else if (op = "mul")
+    {
+        calculate = a * b;
+    }
+
+    console.log(calculate);
 }
