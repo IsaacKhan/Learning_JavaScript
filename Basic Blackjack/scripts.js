f215aa383808d1cfac3d359c96866b9406b11b0b
Isaac Khan <Isaac.Khan@usm.edu>
2019-03-13 15:27:00 -0500

    Made minor formating changes to .html, added JS for check for winstate for houe and player, added JS for displaying score of house and player hand

--- Basic Blackjack/scripts.js
+++ Basic Blackjack/scripts.js
@@ -1,7 +1,16 @@
-// Need to implement a score tracker of some sort. something uber basic
-// Most of this code is for template use later on, so we know what to do and how
-// time for more sleep
+// Notes //
+// 
+//
+//
 
+// Global Variables //
+var houseHand, playerHand, currentDeck;
+var houseCurrentScore, playerCurrentScore;
+var request = new XMLHttpRequest();
+var pRequest = new XMLHttpRequest();
+var hRequest = new XMLHttpRequest();
+
+// Set listeners on load //
 window.onload = function ()
 {
     document.getElementById('start').addEventListener('click', start);
@@ -10,62 +19,23 @@ window.onload = function ()
     document.getElementById('log').addEventListener('click', logData);
 }
 
-var houseHand, playerHand, currentDeck;
-var request = new XMLHttpRequest();
-var pRequest = new XMLHttpRequest();
-var hRequest = new XMLHttpRequest();
-
+// Functions tied to buttons //
 function start()
 {
+    clearHands();
     createDeck();
     showHands();
 }
 
 function hitMe()
 {
-    fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=1')
-        .then(function (response)
-        {
-            return response.json();
-        })
-        .then((data) =>
-            {
-                playerHand.cards.push(data.cards[0]);
-                var card3 = document.getElementById("PlayerC3");
-
-                if (card3.getAttribute('src') == "") 
-                {
-                    document.getElementById("PlayerC3").src = data.cards[0].image;
-                }
-                else
-                {
-                    document.getElementById("PlayerC4").src = data.cards[0].image;
-                }
-
-                document.getElementById("deckcount").textContent = data.remaining;
-
-                // gotta love debugging
-                console.log('Hitting the Player');
-                console.log(`Data: `, data);
-                console.log(`playerHnad: `, playerHand);
-                console.log('-----------------');
-            })
+    hitPlayer();
+    hitHouse();
 }
 
 function stay()
 {
-    console.log("Fine, I'll stay then.");
-}
-
-function showHands()
-{
-    var dHand = document.getElementById("dealer")
-    var pHand = document.getElementById("player")
-
-    if (dHand.style.display === "none") 
-        dHand.style.display = "block";
-    if (pHand.style.display === "none")
-        pHand.style.display = "block";
+    hitHouse();
 }
 
 function logData()
@@ -79,8 +49,28 @@ function logData()
     console.log("Player Hand Info: ", playerHand);
     console.log("-----------------");
     console.log("dealHouse() Data");
-    console.log("Dealer Hand Info: ", houseHand);
+    console.log("House Hand Info: ", houseHand);
     console.log("*****************");
+}
+
+// Functions that support button functions //
+function clearHands()
+{
+    hideHands();
+    document.getElementById("deckcount").textContent = "";
+    document.getElementById("status").textContent = "";
+
+    document.getElementById("houseScore").textContent = "Score goes here";
+    document.getElementById("HouseC1").src = "";
+    document.getElementById("HouseC2").src = "";
+    document.getElementById("HouseC3").src = "";
+    document.getElementById("HouseC4").src = "";
+
+    document.getElementById("playerScore").textContent = "Score goes here";
+    document.getElementById("PlayerC1").src = "";
+    document.getElementById("PlayerC2").src = "";
+    document.getElementById("PlayerC3").src = "";
+    document.getElementById("PlayerC4").src = "";    
 }
 
 function createDeck()
@@ -107,6 +97,28 @@ function createDeck()
         .then(houseHand => dealHouse(currentDeck));
 }
 
+function showHands()
+{
+    var dHand = document.getElementById("house")
+    var pHand = document.getElementById("player")
+
+    if (dHand.style.display === "none") 
+        dHand.style.display = "block";
+    if (pHand.style.display === "none")
+        pHand.style.display = "block";
+}
+
+function hideHands()
+{
+    var dHand = document.getElementById("house")
+    var pHand = document.getElementById("player")
+
+    if (dHand.style.display !== "none") 
+        dHand.style.display = "none";
+    if (pHand.style.display !== "none")
+        pHand.style.display = "none";
+}
+
 function dealPlayer(currentDeck)
 {
     fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=2')
@@ -121,6 +133,7 @@ function dealPlayer(currentDeck)
                 document.getElementById("PlayerC1").src = playerHand.cards[0].image;
                 document.getElementById("PlayerC2").src = playerHand.cards[1].image;
                 document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
 
                 // gotta love debugging
                 console.log('Dealing to player');
@@ -128,6 +141,7 @@ function dealPlayer(currentDeck)
                 console.log(`playerHand: `, playerHand);
                 console.log('-----------------');
             })
+        .then ((data2) => calculateScore("player"))
 }
 
 function dealHouse(currentDeck)
@@ -141,9 +155,10 @@ function dealHouse(currentDeck)
             {
                 houseHand = data;
 
-                document.getElementById("DealerC1").src = houseHand.cards[0].image;
-                document.getElementById("DealerC2").src = houseHand.cards[1].image;
+                document.getElementById("HouseC1").src = houseHand.cards[0].image;
+                document.getElementById("HouseC2").src = houseHand.cards[1].image;
                 document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
 
                 // gotta love debugging
                 console.log('Dealing to House');
@@ -151,4 +166,269 @@ function dealHouse(currentDeck)
                 console.log(`houseHand: `, houseHand);
                 console.log('-----------------');
             })
+            .then ((data2) => calculateScore("house"))
+}
+
+function calculateScore(who)
+{        
+    var score = 0, altScore = 0, currCard;
+    var aceFound = false;
+
+    if(who == "player")
+    {
+        for(var i = 0; i < 4; i++)
+        {
+            currCard = playerHand.cards[i]
+
+            if(currCard == null)
+            {
+                break;
+            }
+            else
+            {
+                if(currCard.value == "KING"||currCard.value == "QUEEN"||currCard.value == "JACK")
+                {
+                    score += 10;
+                    altScore += 10;
+                }
+                else if (currCard.value == "ACE")
+                {
+                    score += 1;
+                    altScore += 11;
+                    aceFound = true;
+                }
+                else
+                {
+                    score += parseInt(currCard.value);
+                    altScore += parseInt(currCard.value);
+                }
+            }
+
+            currCard = null;
+        }  
+
+        checkWinLoseState("player", aceFound, score, altScore);
+    }
+    else if (who == "house")
+    {
+        for(var i = 0; i < 4; i++)
+        {
+            currCard = houseHand.cards[i]
+
+            if(currCard == null)
+            {
+                break;
+            }
+            else
+            {
+                if(currCard.value == "KING"||currCard.value == "QUEEN"||currCard.value == "JACK")
+                {
+                    score += 10;
+                    altScore += 10;
+                }
+                else if (currCard.value == "ACE")
+                {
+                    score += 1;
+                    altScore += 11;
+                    aceFound = true;
+                }
+                else
+                {
+                    score += parseInt(currCard.value);
+                    altScore += parseInt(currCard.value);
+                }
+            }
+
+            currCard = null;
+        }
+
+        checkWinLoseState("house", aceFound, score, altScore);
+    }
+    else
+    {
+        console.log("variable should be either player or house");
+        console.log(hand);
+    }
+}
+
+function checkWinLoseState(hand, aceFound, score, altScore)
+{
+    var winner = false;
+    var altWinner = false;
+    var bust = false;
+
+    if (hand == "player")
+    {
+        if(score == 21 || altScore == 21)
+        {
+            if (altScore > score)
+                altWinner = true;
+            else
+                winner = true;
+        }
+        else if (score > 21)
+            bust = true;
+
+        displayScore("player", aceFound, score, altScore, winner, altWinner, bust);
+    }
+    if (hand == "house")
+    {
+        if(score == 21 || altScore == 21)
+        {
+            if (altScore > score)
+                altWinner = true;
+            else
+                winner = true;
+        }
+        else if (score > 21)
+            bust = true;
+        
+        displayScore("house", aceFound, score, altScore, winner, altWinner, bust);
+    }
+}
+
+function displayScore(hand, aceFound, score, altScore, winner, altWinner, bust)
+{
+    if (hand == "player")
+    {
+        if (altWinner == true)
+            document.getElementById("playerScore").textContent = `You won with a score of ${altScore}`;
+        else if (winner == true)
+            document.getElementById("playerScore").textContent = `You won with a score of ${score}`;
+        else if (bust == true)
+            document.getElementById("playerScore").textContent = `You lost with a score of ${score}`;
+        else
+        {
+            if (aceFound == true)
+                document.getElementById("playerScore").textContent = `Your hand's value is: ${score} or ${altScore}`;
+            else
+                document.getElementById("playerScore").textContent = `Your hand's value is: ${score}`;
+        }
+    }
+    if (hand == "house")
+    {
+        if (altWinner == true)
+            document.getElementById("houseScore").textContent = `You won with a score of ${altScore}`;
+        else if (winner == true)
+            document.getElementById("houseScore").textContent = `You won with a score of ${score}`;
+        else if (bust == true)
+            document.getElementById("houseScore").textContent = `You lost with a score of ${score}`;
+        else
+        {
+            if (aceFound == true)
+                document.getElementById("houseScore").textContent = `Your hand's value is: ${score} or ${altScore}`;
+            else
+                document.getElementById("houseScore").textContent = `Your hand's value is: ${score}`;
+        }
+    }
+}
+
+function hitPlayer()
+{
+    var card3 = document.getElementById("PlayerC3");
+    var card4 = document.getElementById("PlayerC4");
+
+    if (card3.getAttribute('src') == "") 
+    {
+        fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=1')
+        .then(function (response)
+            {
+            return response.json();
+            })
+        .then((data) =>
+            {
+                playerHand.cards.push(data.cards[0]);
+                document.getElementById("PlayerC3").src = data.cards[0].image;
+                document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
+
+                // gotta love debugging
+                console.log('Hitting the Player');
+                console.log(`Data: `, data);
+                console.log(`playerHnad: `, playerHand);
+                console.log('-----------------');
+            })
+        .then ((data2) => calculateScore("player"))
+    }
+    else if (card4.getAttribute('src') == "")
+    {
+        fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=1')
+        .then(function (response)
+            {
+            return response.json();
+            })
+        .then((data) =>
+            {
+                playerHand.cards.push(data.cards[0]);
+                document.getElementById("PlayerC4").src = data.cards[0].image;
+                document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
+
+                // gotta love debugging
+                console.log('Hitting the Player');
+                console.log(`Data: `, data);
+                console.log(`playerHnad: `, playerHand);
+                console.log('-----------------');
+            })
+        .then ((data2) => calculateScore("player"))
+    }
+    else
+    {
+        console.log("player say wut");
+    }
+}
+
+function hitHouse()
+{
+    var card3 = document.getElementById("HouseC3");
+    var card4 = document.getElementById("HouseC4");
+
+    if (card3.getAttribute('src') == "") 
+    {
+        fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=1')
+        .then(function (response)
+            {
+            return response.json();
+            })
+        .then((data) =>
+            {
+                houseHand.cards.push(data.cards[0]);
+                document.getElementById("HouseC3").src = data.cards[0].image;
+                document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
+
+                // gotta love debugging
+                console.log('Hitting the House');
+                console.log(`Data: `, data);
+                console.log(`houseHand: `, houseHand);
+                console.log('-----------------');
+            })
+        .then ((data2) => calculateScore("house"))
+    }
+    else if (card4.getAttribute('src') == "")
+    {
+        fetch('https://deckofcardsapi.com/api/deck/' + currentDeck.deck_id + '/draw/?count=1')
+        .then(function (response)
+            {
+            return response.json();
+            })
+        .then((data) =>
+            {
+                houseHand.cards.push(data.cards[0]);
+                document.getElementById("HouseC4").src = data.cards[0].image;
+                document.getElementById("deckcount").textContent = data.remaining;
+                currentDeck.remaining = data.remaining;
+
+                // gotta love debugging
+                console.log('Hitting the House');
+                console.log(`Data: `, data);
+                console.log(`houseHand: `, houseHand);
+                console.log('-----------------');
+            })
+        .then ((data2) => calculateScore("house"))
+    }
+    else
+    {
+        console.log("house say wut");
+    }
 }
